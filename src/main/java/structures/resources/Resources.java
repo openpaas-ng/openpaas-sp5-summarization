f5bcafa00aef43f47c8e272791c7d1d593bc2946
Tom Jorquera <tom.jorquera@linagora.com>
2017-03-01 11:07:52 +0100

    Add option to get keywords as resources
    The `resources` parameter of the `getCurrentResources` method now accepts a new
    value `keywords` to returns the latest keywords as part of the answer.
    
    To this end, the `Resources` class now has an additional `keywords` field, along
    with related getter and setter.

--- src/main/java/structures/resources/Resources.java
+++ src/main/java/structures/resources/Resources.java
@@ -1,14 +1,24 @@
 package structures.resources;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by midas on 30/12/2016.
  */
 public class Resources {
+    public Map<String, Double> keywords;
     public List<Email> mails;
     public List<StackOverflow> soArticles;
     public List<Wikipedia> wikiArticles;
+
+    public Map<String, Double> getKeywords() {
+        return keywords;
+    }
+
+    public void setKeywords(Map<String, Double> keywords) {
+        this.keywords = keywords;
+    }
 
     public List<StackOverflow> getSoArticles() {
         return soArticles;
